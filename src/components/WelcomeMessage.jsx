d1d7c48629c4fc663f9f15c02dd56b541d89ca8f
geekyarya <119623592+geekyarya@users.noreply.github.com>
2024-09-05 13:28:58 +0530

    Update WelcomeMessage.jsx

--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -1,7 +1,7 @@
 import styles from "./WelcomeMessage.module.css";
 
 const WelcomeMessage = () => {
-  return <p className={styles.welcome}>NAMASTE,Swikk<3</p>;
+  return <p className={styles.welcome}>NAMASTE,Swikk</p>;
 };
 
 export default WelcomeMessage;
